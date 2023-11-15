4403588850f57f124c8b59e844b3a67f5ea24f40
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-11-15 15:24:46 -0300

    Create api for maps

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import doctorRoutes from "./routes/doctorRoute.js";
-import cors from "cors";
+import mapRoutes from "./routes/mapRouter.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/doctorAPI", doctorRoutes);
+app.use("/mapAPI", mapRoutes);
 app.get("/ping", (req, res) => res.send("pong"));
 app.get('/', function (req, res) {
   return res.send("Hello World!");
